48db803cf81e1e4ea85df1f50064bbeb5ba3a19f
ErmesonBiel <ermesongabriel53@gmail.com>
2026-09-17 19:43:42 -0300

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -206,7 +206,7 @@ const historicoList = document.getElementById("historicoPerfil");
         const li = document.createElement("li");
         li.style.padding = "8px 0";
         li.style.borderBottom = "1px solid rgba(255,255,255,0.1)";
-        li.innerHTML = `🎮 <strong>${p.jogador1}</strong> (${p.placar}) <strong>${p.jogador2}</strong> — Vencedor: <span style="color: #4ade80;">${p.vencedor}</span>`;
+        li.innerHTML = ` <strong>${p.jogador1}</strong> (${p.placar}) <strong>${p.jogador2}</strong> — Vencedor: <span style="color: #4ade80;">${p.vencedor}</span>`;
         historicoList.appendChild(li);
       });
     }
